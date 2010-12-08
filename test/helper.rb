ac986a0e49af75c51afee6f1f9a817c2aaef7099
Rafael George <george.rafael@gmail.com>
2010-12-08 19:40:48 +0000

    added test helper with some helper methods

--- test/helper.rb
+++ test/helper.rb
@@ -9,3 +9,27 @@ require 'fakeweb'
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'bucketface'
+
+FakeWeb.allow_net_connect = false
+
+class Test::Unit::TestCase
+end
+
+def fixture_file(filename)
+  return '' if filename == ''
+  file_path = File.expand_path(File.dirname(__FILE__) + '/fixtures/' + filename)
+  File.read(file_path)
+end
+
+def bitbucket_url(url)
+  url =~ /^http/ ? url : 'https://api.bitbucket.org/1.0'
+end
+
+def stub_request(method, url, filename, status=nil)
+  options = {:body => ''}
+  options.merge!({:body => fixture_file(filename)}) if filename
+  options.merge!({:body => status.last}) if status
+  options.merge!({:status => status}) if status
+
+  FakeWeb.register_uri(method, bitbucket_url(url, options))
+end
