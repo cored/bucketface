5a6b4612668d69558d60341979ce50b17176ae65
Rafael George <george.rafael@gmail.com>
2013-06-26 19:50:47 -0400

    Add initial code for listing priviledge groups

--- lib/bucketface.rb
+++ lib/bucketface.rb
@@ -1,5 +1,14 @@
 require "bucketface/version"
+require "faraday"
+require "faraday_middleware"
 
 module Bucketface
-  # Your code goes here...
+
+  def self.list_privileged_groups
+    connection = Faraday.new(:url => 'https://bitbucket.org/api/1.0') do |builder| 
+      builder.adapter Faraday.default_adapter
+      builder.
+    end
+  end
+
 end
--- spec/integration/group_privileges_spec.rb
+++ spec/integration/group_privileges_spec.rb
@@ -1,3 +1,5 @@
+require_relative '../spec_helper'
+
 describe Bucketface, '.list_privileged_groups' do 
   subject { Bucketface.list_privileged_groups }
 
